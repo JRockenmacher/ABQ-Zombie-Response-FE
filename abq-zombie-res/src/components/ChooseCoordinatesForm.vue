70bc94a9af33eab6a7aef06fa50f40b4df0f8b3d
JRocken <jp.rockenmacher@gmail.com>
2018-09-23 09:26:09 -0600

    new project

---
+++ abq-zombie-res/src/components/ChooseCoordinatesForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <b-form inline>
+            <label class="sr-only" for="lat-input">Latitiude</label>
+            <b-input class="mb-2 mr-sm-2 mb-sm-0" id="lat" placeholder="numberic lattitude" />
+            
+            <label class="sr-only" for="long-input">Username</label>
+            <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
+                <b-input id="long-input" placeholder="numberic longitude" />
+            </b-input-group>
+            <b-button variant="primary">Save</b-button>
+        </b-form>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ abq-zombie-res/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div class="top-nav">
+            <h3>Nav Here!</h3>
+        </div>
+        <section class="container">
+            <h1 class="page-title">ABQ Zombie Response Guide</h1>
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
